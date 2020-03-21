2fee4e00936319f1e0d89786b215433ec6a3c6a0
Nicole Sims <nssims105@gmail.com>
2020-03-21 17:07:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Dog 
+  def bark
+    puts "Woof!!!"
+end
+
+def name= (naming_the_dog)
+  @name =naming_the_dog
+end
+
+def name 
+ @name
+end
+
+end
+class Dog
+  def bark
+    puts "Woof!!!#{@name}"
+  end
+  
+fido=Dog.new
+fido.name
+fido.bark
+  
